006ae32692863d074f995cbb87abdca137fb2954
Aaron Benson Wong <a.wong@erasmusmc.nl>
2025-06-17 17:19:03 +0200

    AW_fit_probetrack.py export fit results
    updated AW_fit_probetrack.py to output fit results as a csv file. Removed brainrender code.

--- src/summary/AW_fit_probetrack.py
+++ src/summary/AW_fit_probetrack.py
@@ -1,17 +1,15 @@
 
 # Import functions from the new module
 from utils.csv_utils import get_points_from_csv, ask_csv_file
+import os
+import csv
 import numpy as np
-
-import brainrender
-from brainrender import Scene
-from brainrender.actors import Points,Line
 
 scale_to_um = 1000
 
 # Read points from CSV file ============
-csv_files = ask_csv_file()
-probe_track = get_points_from_csv(csv_files, scale_to_um,marker_name="Type 1")
+csv_file = ask_csv_file()
+probe_track = get_points_from_csv(csv_file, scale_to_um,marker_name="Type 1")
 cells_array = np.array(np.vstack(probe_track))  # Convert means to a NumPy array
 # ========================================
 
@@ -40,30 +38,27 @@ projections = np.dot(centered_points, line_direction)
 deepest_point = np.max(projections)
 probe_length = 787.5
 
-# Generate points along the regression line
-t = np.linspace(deepest_point-probe_length, deepest_point, num=100)  # Parameter for line generation
-regression_line_points = mean_point + t[:, np.newaxis] * line_direction
+# Generate end points in the regression line
+t = np.linspace(deepest_point-probe_length, deepest_point, num=2)  # Parameter for line generation
+regression_line_ends = mean_point + t[:, np.newaxis] * line_direction
+
 
 print(f"Mean point: {mean_point}")
 print(f"Line direction: {line_direction}")
-# Print the regression line points
-print(f"Regression line points: {regression_line_points}")
+print(f"Regression line ends: {regression_line_ends}")
 # ========================================
 
+## Store regression line points in a CSV file ==============
+# Create output filename by appending suffix to the input csv_file
+base, ext = os.path.splitext(csv_file)
+output_csv = f"{base}_fit{ext}"
 
-## Rendering in brainrender ==============
-brainrender.settings.SHADER_STYLE = "plastic"
-
-scene = Scene(title="Labelled points")
-# Add region to scene
-ic = scene.add_brain_region("IC", alpha=0.3)
-
-# ipts = scene.root.mesh.inside_points(regression_line_points).coordinates
-
-scene.add(Points(regression_line_points, name="Probe", colors="black"))
-# scene.add(Line(regression_line_points, name="Probe", linewidth=10, color="black"))
-scene.add(Points(probe_track, name="Track", colors="red"))
-
-scene.content
-scene.render()
+# Save regression_line_ends to CSV
+with open(output_csv, mode='w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(['ccf_ap', 'ccf_dv', 'ccf_ml'])
+    for point in regression_line_ends:
+        writer.writerow(point)
+print(f"Regression line ends saved to: {output_csv}")
 # ========================================
+
